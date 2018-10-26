c3044df508cb7436b88423a9d05692914dd25e7b
zhazhalaila <1176694275@qq.com>
2018-10-26 18:37:31 +0800

    fix database error

--- config.py
+++ config.py
@@ -3,6 +3,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
 	SECRET_KEY = os.environ.get('SECRET_KEY') or 'my-key-that-never-guess'
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
-		'sqlite:///' + os.path.join(basedir, 'app.db')
+	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
