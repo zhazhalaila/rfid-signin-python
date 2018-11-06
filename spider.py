5fd40f2e8fa17061d3fcd197e7c7ffab26124da0
zhazhalaila <1176694275@qq.com>
2018-11-06 15:51:31 +0800

    fix 404 bug

--- spider.py
+++ spider.py
@@ -1,6 +1,15 @@
+import redis, os, base64
 import serial, requests
 import json
 from pusher import Pusher
+
+def encode(key, clear):
+    enc = []
+    for i in range(len(clear)):
+        key_c = key[i % len(key)]
+        enc_c = chr((ord(clear[i]) + ord(key_c)) % 256)
+        enc.append(enc_c)
+    return base64.urlsafe_b64encode("".join(enc).encode()).decode()
 
 ser = serial.Serial('COM3', 9600)
 pusher_client = Pusher(
@@ -11,12 +20,17 @@ pusher_client = Pusher(
   ssl=True
 )
 
+r = redis.StrictRedis(host='localhost', port=6379, db=0)
+token = r.get('curr_token')
+secret = os.environ.get('secret') or 'my_spider_key'
+
 while True:
 	data = ser.readline()
 	if data.startswith(b'Card UID:'):
 		data = data.lstrip(b'Card UID:').strip()
 		print(data)
-		payload = {'rfid_id': data}
+		enc = encode(secret, str(data))
+		payload = {'rfid_id': enc, 'secret': secret}
 		test = requests.get('https://flask-serialshow.herokuapp.com/signin', params=payload)
 		print(test.text)
 		pusher_client.trigger('message', 'send', {'name': test.text})
